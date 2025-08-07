0915c81620326e5383586cfd1aeed7c1ed8ebd07
ali-raza-91 <AliRazaAnsari.code@gmail.com>
2025-08-06 20:56:47 -0700

    Initial commit

--- first/first/settings.py
+++ first/first/settings.py
@@ -22,15 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 
-SECRET_KEY = os.environ.get('SECRET_KEY', 'unsafe-default-secret-key')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-CORS_ALLOWED_ORIGINS = [
-    "https://colopdf.vercel.app",
-]
-
+ALLOWED_HOSTS = ['.railway.app']
 # Application definition
 
 INSTALLED_APPS = [
