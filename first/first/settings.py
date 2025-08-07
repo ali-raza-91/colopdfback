f19f5abb55c1e906316a600b975de415647e9fff
ali-raza-91 <AliRazaAnsari.code@gmail.com>
2025-08-06 21:08:11 -0700

    Initial deploy setup

--- first/first/settings.py
+++ first/first/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -132,10 +133,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# This should be a list
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+# Whitenoise
+MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    # existing middleware...
+]
+
 
 
 MEDIA_URL = '/media/'
